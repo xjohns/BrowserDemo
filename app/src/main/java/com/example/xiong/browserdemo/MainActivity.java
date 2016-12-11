38a12316de560575462de785bdd60c82909c1cab
xjohns <2867193240@qq.com>
2016-12-11 23:57:35 +0800

    进一步优化图标，增加一些功能
    Signed-off-by: xjohns <2867193240@qq.com>

--- app/src/main/java/com/example/xiong/browserdemo/MainActivity.java
+++ app/src/main/java/com/example/xiong/browserdemo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.xiong.browserdemo;
 
+import android.content.Intent;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -12,18 +14,28 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     //声明各组件
-    private Button btn_main;
+    private Button btn_menu;
+    private Button btn_refresh;
+    private Button btn_home;
     private Button btn_back;
     private Button btn_forward;
     private Button btn_search;
+    private Button btn_javaScriptEnabled;
+    private Button btn_imageLoad;
     private WebView wVmain;
     private ProgressBar progressBar;
     private EditText eText;
     private String url_home="http:/www.baidu.com/";
     private String url_input;
+    private Boolean javaScriptEnabled = false;
+    private Boolean imageCanLoad = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +44,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         init();
         //设置好内置浏览器的属性
         WebSettings webSettings = wVmain.getSettings();
-        webSettings.setJavaScriptEnabled(true);//启用支持javascript
+        webSettings.setJavaScriptEnabled(javaScriptEnabled);//启用支持javascript
+        webSettings.supportMultipleWindows();//支持多窗口
+        webSettings.setSupportZoom(true);//启用支持缩放
+        webSettings.setLoadsImagesAutomatically(imageCanLoad);//设置自动加载图片
         wVmain.getSettings().setCacheMode(webSettings.LOAD_CACHE_ELSE_NETWORK);//优先使用缓存
         wVmain.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 wVmain.loadUrl(url);
+                eText.setText(wVmain.getUrl());
                 return true;
             }
 
@@ -56,53 +72,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     progressBar.setProgress(newProgress);
                 }
             }
+
         });
     }
     //构造init方法集中初始化
     private void init() {
-        btn_main = (Button) findViewById(R.id.rbtn_home);
+        btn_menu = (Button) findViewById(R.id.rbtn_menu);
+        btn_refresh = (Button) findViewById(R.id.rbtn_refresh);
+        btn_home = (Button) findViewById(R.id.rbtn_home);
         btn_back = (Button) findViewById(R.id.rbtn_back);
         btn_search = (Button) findViewById(R.id.search);
         btn_forward = (Button) findViewById(R.id.rbtn_forward);
+        btn_javaScriptEnabled = (Button) findViewById(R.id.rbtn_javaScriptEnabled);
+        btn_imageLoad = (Button) findViewById(R.id.rbtn_imageLoad);
         eText = (EditText) findViewById(R.id.editText);
         wVmain = (WebView) findViewById(R.id.wView);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        btn_main.setOnClickListener(this);
+
+        btn_menu.setOnClickListener(this);
+        btn_refresh.setOnClickListener(this);
+        btn_home.setOnClickListener(this);
         btn_back.setOnClickListener(this);
         btn_forward.setOnClickListener(this);
         btn_search.setOnClickListener(this);
+        btn_javaScriptEnabled.setOnClickListener(this);
+        btn_imageLoad.setOnClickListener(this);
         eText.setOnClickListener(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        View v = findViewById(R.id.night_background);
-        v.getBackground().setAlpha(50);//设置背景的透明度
-        switch (getTaskId()){
-            case R.id.menu_night : {
-                v.setVisibility(View.VISIBLE);
-                break;
-            }
-        }
-        return true;
     }
 
     //业务逻辑实现
     @Override
     public void onClick(View v) {
         switch (v.getId()){
+            //设置按钮功能
+            case R.id.rbtn_menu: {
+                Intent intent = new Intent(MainActivity.this, ActivityMenu.class);
+                startActivity(intent);
+                break;
+            }
+            //刷新按钮功能
+            case R.id.rbtn_refresh: {
+                wVmain.reload();
+                break;
+            }
             //首页按钮功能
             case R.id.rbtn_home:{
                 wVmain.loadUrl(url_home);//加载web资源
                 break;
             }
-            //通过EditText转到指定网址功能
+            //通过EditText转到指定网址功能，点击之后调用搜索结束方法改变控件显示
             case R.id.search:{
-                url_input = "http:/" + eText.getText().toString() +"/";
+                url_input = "http://" + eText.getText().toString() +"/";
+                search_close();
                 wVmain.loadUrl(url_input);
-                eText.setText(wVmain.getUrl());
-                btn_search.setVisibility(View.GONE);
                 break;
             }
             //后退按钮功能
@@ -122,30 +144,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             }
+            //点击搜索框时改变控件显示
             case R.id.editText: {
-                eText.setText("");
-                eText.setCursorVisible(true);
-                btn_search.setVisibility(View.VISIBLE);
-                eText.clearFocus();
+                search_start();
+                break;
+            }
+            case R.id.rbtn_javaScriptEnabled: {
+                if (javaScriptEnabled == false){
+                    javaScriptEnabled = true;
+                }else {
+                    javaScriptEnabled = false;
+                }
+                break;
+            }
+            case R.id.rbtn_imageLoad: {
+                if (imageCanLoad = false){
+                    imageCanLoad = true;
+                }else {
+                    imageCanLoad = false;
+                }
                 break;
             }
 
         }
 
     }
+
+    private void search_start () {
+        eText.selectAll();
+        eText.setCursorVisible(true);
+        btn_search.setVisibility(View.VISIBLE);
+    }
+
+    private void search_close () {
+        eText.setCursorVisible(false);
+        btn_search.setVisibility(View.INVISIBLE);
+        eText.setText(wVmain.getUrl());
+    }
+
     //改写物理按键--返回的逻辑
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        System.exit(0);
-//        if (keyCode == KeyEvent.KEYCODE_BACK){
-//            if (wVmain.canGoBack()){
-//                wVmain.goBack();//返回上一页
-//                return true;
-//            }
-//            else {
-//                System.exit(0);//退出程序
-//            }
-//        }
-        return super.onKeyDown(keyCode, event);
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+                wVmain.goBack();
+            exit_doubleClick();
+        }
+        return false;
     }
+    private static Boolean isExit = false;
+    //构造双击返回键退出方法
+    private void exit_doubleClick() {
+        Timer tExit = null;
+        if (isExit == false){
+            isExit = true;//准备退出
+            Toast.makeText(MainActivity.this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            tExit = new Timer();
+            tExit.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    isExit = false; //取消退出
+                }
+            }, 1000);//如果1秒内没有再按一次则取消任务
+        }else {
+            finish();
+            System.exit(0);
+        }
+    }
+
 }
